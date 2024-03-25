0379b35375ff8bf5874c698974f298941a35265b
Yassir-MMI <yassir.ouadghiri@edu.univ-fcomte.Fr>
2024-03-25 19:01:17 +0100

    router

--- src/components/AfficheFilm.vue
+++ src/components/AfficheFilm.vue
@@ -16,9 +16,7 @@ defineProps <Database["public"]["Tables"]["Films"]["Row"] & {Celebrite:Tables<'C
         <div class="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-black group-hover:from-black/70 group-hover:via-black/60 group-hover:to-black/70"></div>
             <div class="absolute inset-0 flex translate-y-[60%] flex-col items-center justify-center px-9 text-center transition-all duration-500 group-hover:translate-y-0">
                 <h1 class="font-dmserif text-3xl font-bold text-white">{{ nom_film }}</h1>
-                <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"> {{ description_card }}</p>
-                <RouterLink :to="`/films/${id}`" class="rounded-full bg-neutral-900 py-2 px-3.5 font-com text-sm capitalize text-white shadow shadow-black/60">Voir plus</RouterLink>
-            </div>
+                <p class="mb-3 text-lg italic text-white opacity-0 transition-opacity duration-300 group-hover:opacity-100"> {{ description_card }}</p>            </div>
         </div>
     </div>
     </RouterLink>   
